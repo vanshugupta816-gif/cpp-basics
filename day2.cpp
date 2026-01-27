bd11147a4fe03ac5a2ab290bd57c4313591ccdf7
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-01-27 18:52:04 +0530

    Create day2.cpp

---
+++ day2.cpp
@@ -0,0 +1,14 @@
+// ****VARIABLES & COMMENTS IN C++****
+
+#include<iostream>
+using namespace std;
+// this program was made by vanshika
+/*this 
+is a 
+multiple line 
+comment*/ 
+int main(){
+    int sum=6;
+    cout<<"hi g"<<"\n"<<sum;
+    return 0;
+}

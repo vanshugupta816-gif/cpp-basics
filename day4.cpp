c9d0e76b505357e16fc05c537646d42e5edfddfb
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-01-28 21:27:02 +0530

    Create day4.cpp

---
+++ day4.cpp
@@ -0,0 +1,18 @@
+// ****C++ BASIC INPUT/OUTPUT & MORE****
+
+# include <iostream>
+using namespace std;
+
+int main() 
+{
+int num1,  num2;
+cout<<"enter the value of num1:\n";/*'<<' is insertion operator*/
+cin>>num1;/*'>>' is extraction operator*/
+
+cout<<"enter the value of num2:\n";/*'<<' is insertion operator*/
+cin>>num2;/*'>>' is extraction operator*/
+
+cout<<"the sum is"<< num1+num2;
+
+return 0;
+}

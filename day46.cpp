76f59f97ef373cea259a6761be3cfa98cd280af3
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-02-19 23:15:17 +0530

    Create day46.cpp

---
+++ day46.cpp
@@ -0,0 +1,38 @@
+// ****POINTERS TO OBJECTS AND ARROW OPERATOR IN CPP ****
+
+#include<iostream>
+using namespace std;
+
+class complex{
+     int real , imaginary;
+     public:
+     void getdata(){
+        cout<<"the real part is "<<real<<endl;
+        cout<<"the imaginary part is"<<imaginary<<endl;
+     }
+
+     void setdata(int a, int b){
+        real = a;
+        imaginary = b;
+        }
+
+};
+int main(){
+   //  complex c1;
+   //  complex *ptr = &c1;
+   complex *ptr = new complex; 
+    // (*ptr).setdata(1,4);   is exactly same as
+    ptr->setdata(1,4);
+
+   //  (*ptr).getdata(); is as good as
+   ptr->getdata();
+
+
+   // array of objects
+    complex *ptr1 = new complex[4]; 
+    ptr1->setdata(1,4);
+    ptr1->getdata();
+    return 0;
+}
+
+

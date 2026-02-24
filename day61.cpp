d78387ae090f760ce227dd0a9ad1417917e89c01
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-02-24 18:11:54 +0530

    Create day61.cpp

---
+++ day61.cpp
@@ -0,0 +1,25 @@
+// *****INTRODUCTION TO STL*****     ---> competitive programming
+// STL -> standard template library              limited time -> . resize
+//                                                               . sort
+//   library of what?                                            . search
+//              |
+//              |
+//          generic classes and functions.
+//  why use STL? --> REUSE: well tested components 
+//              --> time saving!
+    
+// ****COMPONENTS OF STL****
+// 1. containers :
+//     --> stores data 
+//     --> use template classes
+// 2. Algorithm  :
+//       --> sorting
+//       --> searching
+//       --> use template function
+// 3. Iterators  :
+//        --> object points to an element in a container.
+//         --> handled just like painter.
+//         --> connects algorithm with containers.
+
+// STL is used because it is a good idea not to reinvent the wheel.
+
